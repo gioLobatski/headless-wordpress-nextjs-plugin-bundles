c705c01dee722cc14558a7cb1d66c696809e3772
Gio Lobaton <giolobaton24@gmail.com>
2026-05-14 14:13:15 +0800

    Add wpgraphql-smart-cache plugin

---
+++ bundled-plugins/wpgraphql-smart-cache/wpgraphql-smart-cache/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wp-graphql/wp-graphql-smart-cache',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '652e7e514ee7ba053b59f3b8d4c03df064ec8891',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'appsero/client' => array(
+            'pretty_version' => 'v2.0.6',
+            'version' => '2.0.6.0',
+            'reference' => '2e553be6a47b387a7b05496f6a6e8d8595dd0d08',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../appsero/client',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wp-graphql/wp-graphql-smart-cache' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '652e7e514ee7ba053b59f3b8d4c03df064ec8891',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

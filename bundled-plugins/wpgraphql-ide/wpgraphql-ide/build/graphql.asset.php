c2598816cc97247d222d584613cc05a1ccf3a979
Gio Lobaton <giolobaton24@gmail.com>
2026-05-14 14:13:01 +0800

    Add wpgraphql-ide plugin

---
+++ bundled-plugins/wpgraphql-ide/wpgraphql-ide/build/graphql.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '50d811d1e3e585eb4b0e');
---
+++ bundled-plugins/wpgraphql-ide/wpgraphql-ide/plugins/help-panel/build/help-panel.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-element', 'wp-primitives'), 'version' => '48e1c0a03702e987a92d');

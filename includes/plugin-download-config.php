1ebea1b64640258c1a50e9aaaa695576716bdeba
Gio Lobaton <giolobaton24@gmail.com>
2026-05-18 11:29:47 +0800

    Update config to use v1.2.0 release with source archives

--- includes/plugin-download-config.php
+++ includes/plugin-download-config.php
@@ -20,25 +20,25 @@ return array(
     // E-Commerce
     'woocommerce' => array(
         'zip_file' => 'woocommerce.zip',
-        'version' => 'v1.1.0',
+        'version' => 'v1.2.0',
     ),
     
     // GraphQL
     'wp-graphql' => array(
         'zip_file' => 'wp-graphql.zip',
-        'version' => 'v1.1.0',
+        'version' => 'v1.2.0',
     ),
     'wpgraphql-acf' => array(
         'zip_file' => 'wpgraphql-acf.zip',
-        'version' => 'v1.1.0',
+        'version' => 'v1.2.0',
     ),
     'wpgraphql-ide' => array(
         'zip_file' => 'wpgraphql-ide.zip',
-        'version' => 'v1.1.0',
+        'version' => 'v1.2.0',
     ),
     'wpgraphql-smart-cache' => array(
         'zip_file' => 'wpgraphql-smart-cache.zip',
-        'version' => 'v1.1.0',
+        'version' => 'v1.2.0',
     ),
     // 'wp-graphql-woocommerce' => array(  // NOT IN RELEASE YET
     //     'zip_file' => 'wp-graphql-woocommerce.zip',
@@ -46,7 +46,7 @@ return array(
     // ),
     'wp-graphql-tax-query' => array(
         'zip_file' => 'wp-graphql-tax-query-develop.zip',
-        'version' => 'v1.1.0',
+        'version' => 'v1.2.0',
     ),
     // 'wp-graphql-rank-math' => array(  // NOT IN RELEASE YET
     //     'zip_file' => 'wp-graphql-rank-math.zip',
@@ -56,50 +56,50 @@ return array(
     // SEO
     'seo-by-rank-math' => array(
         'zip_file' => 'seo-by-rank-math.zip',
-        'version' => 'v1.1.0',
+        'version' => 'v1.2.0',
     ),
     
     // Security
     'ithemes-security-pro' => array(
         'zip_file' => 'ithemes-security-pro.zip',
-        'version' => 'v1.1.0',
+        'version' => 'v1.2.0',
     ),
     
     // Backup & Management
     'wp-time-capsule' => array(
         'zip_file' => 'wp-time-capsule.zip',
-        'version' => 'v1.1.0',
+        'version' => 'v1.2.0',
     ),
     'iwp-client' => array(
         'zip_file' => 'iwp-client.zip',
-        'version' => 'v1.1.0',
+        'version' => 'v1.2.0',
     ),
     
     // Content & Media
     'advanced-custom-fields-pro' => array(
         'zip_file' => 'advanced-custom-fields-pro.zip',
-        'version' => 'v1.1.0',
+        'version' => 'v1.2.0',
     ),
     'classic-editor' => array(
         'zip_file' => 'classic-editor.zip',
-        'version' => 'v1.1.0',
+        'version' => 'v1.2.0',
     ),
     'duplicate-page' => array(
         'zip_file' => 'duplicate-page.zip',
-        'version' => 'v1.1.0',
+        'version' => 'v1.2.0',
     ),
     'svg-support' => array(
         'zip_file' => 'svg-support.zip',
-        'version' => 'v1.1.0',
+        'version' => 'v1.2.0',
     ),
     'imagify' => array(
         'zip_file' => 'imagify.zip',
-        'version' => 'v1.1.0',
+        'version' => 'v1.2.0',
     ),
     
     // Forms
     'gravityforms' => array(
         'zip_file' => 'gravityforms.zip',
-        'version' => 'v1.1.0',
+        'version' => 'v1.2.0',
     ),
 );
